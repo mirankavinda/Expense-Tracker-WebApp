cbc247b20ffeee5da5b38cbfcb33dbbea01978be
Miran <mirankavinda12@gmail.com>
2024-03-12 12:21:31 +0530

    Add AddTransaction com

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -7,7 +7,7 @@ export const AddTransaction = () => {
   return (
       <>
           <h3>Add new transaction</h3>
-          <form onSubmit={onSubmit}>
+          <form>
               <div className="form-control">
                   <label htmlFor="text">Text</label>
                   <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
